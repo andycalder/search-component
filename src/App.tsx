8dda3c929b33d3be9ea5d53c02f6eef43469711e
Andrew Calder <andrew.calder@uqconnect.edu.au>
2021-08-07 12:08:52 +1000

    Render search bar with active/inactive state

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-function App() {
+const Container = styled.div`
+  width: 392px;
+  height: 48px;
+  position: relative;
+`;
+
+const Input = styled.input`
+  font-size: 16px;
+  width: 100%;
+  height: 100%;
+  border: 1px solid transparent;
+  border-radius: 24px;
+  box-shadow: 0 2px 5px 1px rgb(64 60 67 / 16%);
+  outline: 0;
+  padding-left: 40px;
+`;
+
+const Controls = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 48px;
+  height: 48px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Results = styled.div`
+  width: 500px;
+  height: 500px;
+  background-color: black;
+`;
+
+const Search = () => {
+  const [active, setActive] = useState(false);
+
+  const toggleActive = () => {
+    setActive(!active);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Controls>
+          <FontAwesomeIcon 
+            icon={active ? "chevron-left" : "search"}
+            onClick={() => toggleActive()}
+          />
+        </Controls>
+        <Input 
+          type="search"
+          placeholder="Find a trail"
+          onClick={() => toggleActive()}
+        />
+      </Container>
+      {active && (<Results></Results>)}
+    </>
   );
-}
+};
 
-export default App;
+export default Search;
